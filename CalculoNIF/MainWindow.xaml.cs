0b633df7e71237ea3c9f1cfd23f19404ad706366
Ken Arias <kenarias77@gmail.com>
2023-12-04 14:29:22 +0100

    subida expenseIt y calculo nif

---
+++ CalculoNIF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculoNIF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Convertir_Click(object sender, RoutedEventArgs e)
+        {
+            int numero=Convert.ToInt32(textBoxNumero.Text)/23;
+            String[] letras = { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E" };
+            textBoxLetra.Text = "la letra es: "+letras[Convert.ToInt32(textBoxNumero.Text) - (numero * 23)];
+        }
+
+    }
+}
